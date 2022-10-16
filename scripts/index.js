b03afd0bda2f622c40f23154843e2a860ee90477
Kalina Frontend <dzhiltcov@yandex.ru>
2022-10-16 12:15:15 +0200

    fix: исправить замечания ревьюера

--- scripts/index.js
+++ scripts/index.js
@@ -3,34 +3,32 @@ let likeButton = document.querySelectorAll('.element__item-like');
 let closeButton = document.querySelector('.popup__close-button');
 let addButton = document.querySelector('.popup__save-button');
 let editButton = document.querySelector('.profile__edit-button');
-
-/*Ставим лайк фото с фотографиями или убираем лайк*/
-likeButton.forEach(likeButton =>
-  likeButton.addEventListener ('click', function () {
-    likeButton.classList.toggle('element__item-like_type_active');
-    }
-))
+let formPopup = document.querySelector('.popup__form-profile');
+let nameInput = formPopup.querySelector('.popup__input_type_name')
+let jobInput = formPopup.querySelector('.popup__input_type_job')
+let profileName = document.querySelector('.profile__info-name');
+let profileJob = document.querySelector('.profile__job');
 
 // Открыть форму PopUp
-editButton.addEventListener ('click', function() {
+function popupOpen() {
   popup.classList.add('popup_opened');
-});
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+
+};
+editButton.addEventListener ('click', popupOpen);
+
 // Закрыть форму PopUp
 function popupClose() {
   popup.classList.remove('popup_opened');
 };
 closeButton.addEventListener ('click', popupClose);
 
-// Изменить данные в профиле
-let formPopup = document.querySelector('.popup__form-profile');
-let nameInput = formPopup.querySelector('.popup__input_type_name')
-let jobInput = formPopup.querySelector('.popup__input_type_job')
-function formSubmitHandler(e) {
-    e.preventDefault();
-    let profileName = document.querySelector('.profile__info-name');
-    let profileJob = document.querySelector('.profile__job');
+// Отправить форму
+function formSubmitHandler(evt) {
+    evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
     popupClose();
 }
-addButton.addEventListener('click', formSubmitHandler);
+formPopup.addEventListener('submit', formSubmitHandler);
