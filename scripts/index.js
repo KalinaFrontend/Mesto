9b8e78c39c4eacccf5e87bfc415413908492d897
Kalina Frontend <dzhiltcov@yandex.ru>
2022-10-25 21:43:36 +0200

    feat: Добавить все popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,52 @@
-let popup = document.querySelector('.popup');
-let likeButton = document.querySelectorAll('.element__item-like');
-let closeButton = document.querySelector('.popup__close-button');
-let addButton = document.querySelector('.popup__save-button');
-let editButton = document.querySelector('.profile__edit-button');
-let formPopup = document.querySelector('.popup__form-profile');
-let nameInput = formPopup.querySelector('.popup__input_type_name')
-let jobInput = formPopup.querySelector('.popup__input_type_job')
-let profileName = document.querySelector('.profile__info-name');
-let profileJob = document.querySelector('.profile__job');
+/* popups */
+const popupProfile = document.querySelector('.popup_type_edit-profile');
+const popupAddElement = document.querySelector('.popup_type_add-element');
+/* buttons */
+const buttonEdit = document.querySelector('.profile__edit-button');
+const buttonAdd = document.querySelector('.profile__add-button');
+const buttonCloseProfile = document.querySelector('.popup__close-button_place_edit-profile');
+const buttonCloseAddElement = document.querySelector('.popup__close-button_place_add-element');
+
+const likeButton = document.querySelectorAll('.element__item-like');
+const saveButton = document.querySelector('.popup__save-button');
+
+
+const formPopup = document.querySelector('.popup__form-profile');
+const nameInput = formPopup.querySelector('.popup__input_type_name')
+const jobInput = formPopup.querySelector('.popup__input_type_job')
+const profileName = document.querySelector('.profile__info-name');
+const profileJob = document.querySelector('.profile__job');
 
 // Открыть форму PopUp
-function popupOpen() {
-  popup.classList.add('popup_opened');
+function popupOpen(popupWindowOpen) {
+  popupWindowOpen.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 
 };
-editButton.addEventListener ('click', popupOpen);
+
+
+buttonEdit.addEventListener('click', () => {
+  popupOpen(popupProfile);
+});
+buttonAdd.addEventListener('click', () => {
+  popupOpen(popupAddElement);
+});
+
+
 
 // Закрыть форму PopUp
-function popupClose() {
-  popup.classList.remove('popup_opened');
+function popupClose(popupWindowClose) {
+  popupWindowClose.classList.remove('popup_opened');
 };
-closeButton.addEventListener ('click', popupClose);
+
+buttonCloseProfile.addEventListener('click', ()  => {
+  popupClose(popupProfile);
+});
+buttonCloseAddElement.addEventListener('click', () => {
+  popupClose(popupAddElement);
+});
+
 
 // Отправить форму
 function formSubmitHandler(evt) {
